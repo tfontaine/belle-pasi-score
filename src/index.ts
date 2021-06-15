0f74d26ded05379acd513cac1f9f8bcef4ed2f37
Thomas Fontaine <tfontaine@grove.co>
2021-06-15 02:14:55 +0000

    First commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('belle-pasi-score')
+export class BellePasiScore extends LitElement {
+  @property() public count = 0;
+
+  static styles = css`
+      * {
+        font-size: 200%;
+      }
+
+      span {
+        width: 4rem;
+        display: inline-block;
+        text-align: center;
+      }
+
+      button {
+        width: 4rem;
+        height: 4rem;
+        border: none;
+        border-radius: 10px;
+        background-color: seagreen;
+        color: white;
+      }`;
+
+  render() {
+    return html`
+      <button @click="${this.dec}">-</button>
+      <span>${this.count}</span>
+      <button @click="${this.inc}">+</button>
+    `;
+  }
+
+  inc() {
+    this.count++;
+  }
+
+  dec() {
+    this.count--;
+  }
+}
---
+++ stories/index.stories.js
@@ -0,0 +1,9 @@
+import "../src/index.ts";
+
+export default {
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const story1 = () => '<belle-pasi-score></belle-pasi-score>';
