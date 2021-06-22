35b452248a517ce1c2ec48877a6df1deccaeef3c
Thomas Fontaine <tfontaine@grove.co>
2021-06-22 03:39:13 +0000

    input box defined by background image

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export class BellePasiScore extends LitElement {
    * Initial caption.
    */
   @property({ attribute: 'uploader-caption', type: String })
-  uploaderCaption = "Drop image here or click to select";
+  uploaderCaption = "";
 
   /**
    * Link to uploader image.
@@ -72,14 +72,23 @@ export class BellePasiScore extends LitElement {
       opacity: 0.3;
     }
 
+    .panel {
+        position: relative;
+        display: inline-block;
+    }
+
     .uploader {
+      position: absolute;
+      z-index: 999;
+      top: 38%;
+      left: 4%;
       font-size: 1rem;
       color: #666666;
       cursor: pointer;
-      width: 25rem;
-      height: 15rem;
-      background: #fff;
-      border: 0.1rem dashed #838388;
+      width: 340px;
+      height: 173px;
+      background: transparent;
+      border: none;
       border-radius: 0.4rem;
       display: flex;
       justify-content: center;
@@ -87,7 +96,7 @@ export class BellePasiScore extends LitElement {
       flex-direction: column;
       margin: 1rem 0 2rem 0;
     }
-
+ 
     .uploader.dragover {
       color: #eeeeee;
       border: 0.1rem solid rgb(0, 120, 212);
@@ -104,15 +113,15 @@ export class BellePasiScore extends LitElement {
       box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19);
     }
   }`
-
+ 
   render() {
     return html`
       <div class="panel">
-        <img src=${this.uploaderBackground} height="600px" width="300px">
+        <img src=${this.uploaderBackground} height="835px" width="373px" />
         <input id="image-uploader" class="hidden" type="file" accept="*" @change=${this._imageDrop} />
   	    <label for="image-uploader" id="image-dragover" class=${this.isDragover ? "uploader dragover" : "uploader"}
           @dragover=${this._imageDragOver} @dragleave=${this._imageDragOver} @drop=${this._imageDrop}>
-    	    <div id="upload-caption">${this.uploaderCaption}</div>
+          <div id="upload-caption">${this.uploaderCaption}</div>
     	      <img id="preview" class="hidden" />
         </label>
       </div>
